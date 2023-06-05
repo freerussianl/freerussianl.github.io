402de3dbb2a63a39a1f6264455d97ba5aa8266ea
Vladimir Ivanov <intrustonly@gmail.com>
2023-06-05 16:58:18 +0200

    About block added

--- src/App.svelte
+++ src/App.svelte
@@ -24,31 +24,102 @@
   let donateRef;
 
   const scrollToDonate = () => {
-    donateRef.scrollIntoView({ behavior: 'smooth' });
-  }
+    donateRef.scrollIntoView({ behavior: "smooth" });
+  };
 </script>
 
 <main>
   <header>
-    <img
-      src="/images/logo_full.svg"
-      alt="Free Russia NL logo"
-      class="logo"
-    />
+    <img src="/images/logo_full.svg" alt="Free Russia NL logo" class="logo" />
     <div class="button_container">
-    <button class="header_donate" on:click={scrollToDonate}><img
-      src="/images/header_donate.svg"
-      alt="Donate"
-      class="img_donate"
-    /></button>
-    <button class="header_language">NL</button>
+      <button class="header_donate" on:click={scrollToDonate}
+        ><img
+          src="/images/header_donate.svg"
+          alt="Donate"
+          class="img_donate"
+        /></button
+      >
+      <button class="header_language">NL</button>
     </div>
   </header>
+  <div>
+    <div class="genesis">
+      <h4 class="title">ontstaansgeschiedenis</h4>
+      <h4 class="bold">
+        Van burgerinitiatief tot geregistreerde ANBI-stichting in Nederland
+      </h4>
+      <p class="text">
+        Free Russia NL is een grassroots community van Russisch-sprekende
+        inwoners en burgers van Nederland. Onze gemeenschap ontstond als een
+        burgerinitiatief in januari 2021. 24 februari 2022 veranderde voor ieder
+        van ons, binnen en buiten Rusland, het leven drastisch. Gedurende de
+        oorlogsmaanden voeren we actie, zamelen geld in voor humanitaire hulp
+        aan Oekraïne en aan de Oekraïense vluchtelingen in Nederland, helpen
+        journalisten en activisten uit Rusland die genoodzaakt zijn het land te
+        verlaten en spreken met Nederlandse media. In juni 2022 hebben we onze
+        organisatie als stichting in Nederland geregistreerd.
+      </p>
+    </div>
+    <img src="/images/community.png" alt="Community" class="img_community" />
+    <div class="about">
+      <h4 class="title">Over ons</h4>
+      <h4 class="bold">
+        Het verenigen, ondersteunen en behartigen van de belangen van
+        Russischtaligen die de democratische waarden in Nederland delen
+      </h4>
+      <p class="text">
+        Wij verlenen humanitaire hulp en organiseren evenementen om de aandacht
+        te vestigen op problemen van vluchtelingen uit Rusland, Oekraïne en
+        Wit-Rusland. Onze educatieve activiteiten zijn gericht op het
+        verspreiden van waarheidsgetrouwe informatie over de Russische politiek
+        en samenleving.
+      </p>
+    </div>
+    <div class="about">
+      <h4 class="title">De doelstellingen</h4>
+      <h4 class="bold">
+        Het bevorderen van democratie, mensenrechten en de rechtsstaat, in het
+        algemeen, meer in het bijzonder:
+      </h4>
+      <p class="text">
+        <li>
+          democratische waarden, -normen en -aanpak, met inbegrip van vrije
+          verkiezingen;
+        </li>
+        <li>
+          mensenrechten, waaronder vrijheid van meningsuiting, vrijheid van
+          vergadering en vereniging, vrijheid van gedachten en van geweten;
+        </li>
+        <li>tolerantie en non-discriminatie;</li>
+        <li>rechtsstaat en onafhankelijkheid van de rechterlijke macht.</li>
+        Het bijeenbrengen en ondersteunen van de in Nederland en Europa gevestigde
+        Russischtalige gemeenschap die de bovengenoemde waarden deelt.
+      </p>
+    </div>
+  </div>
+  <div>
+    <h1>Bestuursleden</h1>
+    <div class="about">
+      <h4 class="bold">
+        Svetlana Pylaeva -
+        <h4 class="title">voorzitter</h4>
+      </h4>
+      <h4 class="bold">
+        Kristina Petrasova -
+        <h4 class="title">secretaris</h4>
+      </h4>
+      <h4 class="bold">
+        Ira Heuvelman -
+        <h4 class="title">penningmeester</h4>
+      </h4>
+    </div>
+  </div>
   <p>
     <a
       href="https://www.change.org/p/декларация-российских-демократических-сил"
     >
-      Verklaring van de Russische Democratische Initiatieven, Berlijn, 30 april 2023
+      Verklaring van de Russische Democratische Initiatieven, Berlijn, 30 april
+      2023
     </a>
   </p>
   <p>
@@ -61,18 +132,20 @@
     </a>
   </p>
   <p>
-    Free Russia NL is een grassroots community van Russisch-sprekende inwoners 
-    en burgers van Nederland. Onze gemeenschap ontstond in januari 2021. 
-    24 februari 2022 veranderde voor ieder van ons, binnen en buiten Rusland, 
-    het leven drastisch. 'Overnight' steeg ons ledenbestand tot 1200 leden - 
-    voor ons een teken van grote behoefte in de Russisch-sprekende gemeenschap 
-    in Nederland om zich tegen de oorlog en tegen het regime uit te spreken, 
-    steun te betuigen aan Oekraïne en elkaar. 
-    Op dit moment telt ons telegramkanaal <a href="https://t.me/FreeRussiaNL">https://t.me/FreeRussiaNL</a>  rond de 2.500 leden. 
-    
-
+    Free Russia NL is een grassroots community van Russisch-sprekende inwoners
+    en burgers van Nederland. Onze gemeenschap ontstond in januari 2021. 24
+    februari 2022 veranderde voor ieder van ons, binnen en buiten Rusland, het
+    leven drastisch. 'Overnight' steeg ons ledenbestand tot 1200 leden - voor
+    ons een teken van grote behoefte in de Russisch-sprekende gemeenschap in
+    Nederland om zich tegen de oorlog en tegen het regime uit te spreken, steun
+    te betuigen aan Oekraïne en elkaar. Op dit moment telt ons telegramkanaal <a
+      href="https://t.me/FreeRussiaNL">https://t.me/FreeRussiaNL</a
+    > rond de 2.500 leden.
   </p>
-  <p>Ook zien we grote belangstelling voor ons standpunt vanuit de Nederlandse samenleving.</p>
+  <p>
+    Ook zien we grote belangstelling voor ons standpunt vanuit de Nederlandse
+    samenleving.
+  </p>
   <p>
     Gedurende de oorlogsmaanden voeren we actie, zamelen geld in voor
     humanitaire hulp aan Oekraïne en aan de Oekraïense vluchtelingen in
@@ -80,8 +153,10 @@
     zijn het land te verlaten en spreken met Nederlandse media. In juni 2022
     hebben we onze organisatie als stichting in Nederland geregistreerd:
   </p>
-    <!-- Donate section content here -->
-  <p bind:this={donateRef}><b>Beneficiary name:</b> <code>Free Russia Nederland</code></p>
+  <!-- Donate section content here -->
+  <p bind:this={donateRef}>
+    <b>Beneficiary name:</b> <code>Free Russia Nederland</code>
+  </p>
   <p><b>IBAN:</b> <code>NL20ABNA0113016549</code></p>
   <p>Wij zijn een ANBI Stichting (KvK nummer: 86836935)</p>
   <p><a href="https://freerussia.nl/tikkie">Doneer via tikkie</a></p>
@@ -110,7 +185,11 @@
     <li>Ira Heuvelman (penningmeester)</li>
   </ul>
 
-  <p>Communication, press and public events: <a href="mailto:press@freerussia.nl">press@freerussia.nl</a></p>
+  <p>
+    Communication, press and public events: <a href="mailto:press@freerussia.nl"
+      >press@freerussia.nl</a
+    >
+  </p>
 
   <p>De doelstellingen van FreeRussiaNL Stichting zijn:</p>
   <p>
@@ -214,8 +293,7 @@
       </li>
     {/each}
   </ul>
-  <h2>
-    and media</h2>
+  <h2>and media</h2>
   <ul>
     {#each press as { date, text, link, comment }}
       <li>
