195eeb52c3f06ef49b64bb5aef4a83cdcf052633
Ivan Kashirin <kashirinivan@icloud.com>
2025-08-02 20:04:56 +0200

    fix: env file path

--- backend/src/config.py
+++ backend/src/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class BaseConfig(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file="../.env",
+        env_file="../../.env",
         env_ignore_empty=True,
         extra="ignore",
     )
